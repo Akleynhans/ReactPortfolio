61be9f4d11cb8ebaf1ade70158f92a4de79ee5b4
Amy Kleynhans <amykleynhans@yahoo.com>
2023-12-01 08:45:21 -0700

    populated page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// import images
+
+// export to page
+export default function Home() {
+
+    return (
+        <div>
+            <h1>AMY KLEYNHANS</h1>
+            <h2>
+                Hi, Im Amy! I graduated from Colorado School of Mines in 2018 with a bacholers of science in Mechanical Engineering. I have been working in the automation field for 5 years now, as a New Product Developement Design Engineer.
+            </h2>
+        </div>
+    );
+}
