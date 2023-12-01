344dcc8a146d95f966698cd54ca4d8bebf78dddb
Amy Kleynhans <amykleynhans@yahoo.com>
2023-12-01 09:19:30 -0700

    imported images

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,7 @@
+// import images
+import Email from '../assets/email.png'
+import Insta from '../assets/insta.png'
+import Github from '../assets/github.png'
 
 export default function Contact() {
     return (
@@ -5,9 +9,9 @@ export default function Contact() {
             <h1>Contact Me</h1>
             <div>
             <ul class="contact-list">
-                    <li><a href="mailto: amykleynhans@yahoo.com"><img class="logo" src="./assets/email.png" alt="email logo"/> </a></li>
-                    <li><a href="https://www.instagram.com/amykleynhans"> <img class="logo" src="./assets/insta.png" alt="instagram logo"/></a></li>
-                    <li><a href="https://github.com/Akleynhans"> <img class="logo" src="./assets/github.png" alt="github logo"/></a></li>
+                    <li><a href="mailto: amykleynhans@yahoo.com"><img class="logo" src={Email} alt="email logo"/> </a></li>
+                    <li><a href="https://www.instagram.com/amykleynhans"> <img class="logo" src={Insta} alt="instagram logo"/></a></li>
+                    <li><a href="https://github.com/Akleynhans"> <img class="logo" src={Github} alt="github logo"/></a></li>
                 </ul>
             </div>
         </div>
