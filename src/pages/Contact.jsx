e23a10375044871817a736821d2e37d356314af8
Amy Kleynhans <amykleynhans@yahoo.com>
2023-12-01 08:55:47 -0700

    popuated contact page and added logos

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <div>
+            <ul class="contact-list">
+                    <li><a href="mailto: amykleynhans@yahoo.com"><img class="logo" src="./assets/email.png" alt="email logo"/> </a></li>
+                    <li><a href="https://www.instagram.com/amykleynhans"> <img class="logo" src="./assets/insta.png" alt="instagram logo"/></a></li>
+                    <li><a href="https://github.com/Akleynhans"> <img class="logo" src="./assets/github.png" alt="github logo"/></a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
